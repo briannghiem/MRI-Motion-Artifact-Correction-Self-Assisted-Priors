0cf8c8e3d097bd431c4b6e7a71c961b5733911e5
briannghiem <brian.nghiem@mail.utoronto.ca>
2022-07-27 22:54:24 -0400

    removed test inference portions

--- train.py
+++ train.py
@@ -14,9 +14,6 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'#'0,1,2,3,4,5,6,7'
 
 # -------------------------------------------------------
-Train = 1 # True False
-Test  = 0 # True False
-# -------------------------------------------------------
 nb_epoch      = 50
 learningRate  = 0.001 # 0.001
 optimizer     = Adam(learning_rate=learningRate)
@@ -77,47 +74,27 @@ def main():
 	valid_after = np.load(spath + r"/val/after_val.npy")
 	valid_label = np.load(spath + r"/val/current_val_GT.npy")
 	#
-	if Train:
-		print('---------------------------------')
-		print('Model Training ...')
-		print('---------------------------------')
-		#
-		model = Correction_Multi_input(Height, Width)
-		print(model.summary())
-		csv_logger = CSVLogger(Weights_path+r'/Loss_Acc.csv', append=True, separator=' ')
-		reduce_lr = LearningRateScheduler(scheduler)
-		model.compile(loss=ssim_loss, optimizer=optimizer, metrics=[ssim_score,'mse'])
-		hist = model.fit(x = [train_before, train_data, train_after],  # train_CE
-						y = train_label,
-						batch_size = batch_size,
-						shuffle = True,#False,
-						epochs = nb_epoch, #100,
-						verbose = 2,          # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch
-						validation_data=([valid_before, valid_data, valid_after], valid_label),   # test_CE
-						callbacks=[csv_logger, reduce_lr])
-		print('Saving Model...')
-		save_model(Weights_path, model,'CorrectionUNet_') # to save the weight - 'CNN_iter_'+str(i)
-		#
-	if Test:
-		# Load the model
-		print('========================================Load Model Weights=====================================')
-		model = load_model(Weights_path, 'CorrectionUNet_') # to load the weight
-		print('---------------------------------')
-		print('Evaluate Model on Testing Set ...')
-		print('---------------------------------')
-		#pred = model.predict(test_data)
-		pred = model.predict([fold1_test_before, test_data, fold1_test_after])  # test_CE
-		print('==================================')
-		print('Predictions=',pred.shape)
-		print('==================================')
-		#
-		# To save reconstructed data:
-		inps = sorted(glob.glob(os.path.join(test_data_path, "*.png")))
-		assert type(inps) is list
-		for i, inp in enumerate(inps):
-			out_fname = os.path.join(Prediction_path, os.path.basename(inp))
-			out_img = pred[i,:,:,:]
-			cv2.imwrite(out_fname, out_img)
+	print('---------------------------------')
+	print('Model Training ...')
+	print('---------------------------------')
+	#
+	model = Correction_Multi_input(Height, Width)
+	print(model.summary())
+	csv_logger = CSVLogger(Weights_path+r'/Loss_Acc.csv', append=True, separator=' ')
+	reduce_lr = LearningRateScheduler(scheduler)
+	model.compile(loss=ssim_loss, optimizer=optimizer, metrics=[ssim_score,'mse'])
+	hist = model.fit(x = [train_before, train_data, train_after],  # train_CE
+					y = train_label,
+					batch_size = batch_size,
+					shuffle = True,#False,
+					epochs = nb_epoch, #100,
+					verbose = 2,          # Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch
+					validation_data=([valid_before, valid_data, valid_after], valid_label),   # test_CE
+					callbacks=[csv_logger, reduce_lr])
+	print('Saving Model...')
+	save_model(Weights_path, model,'CorrectionUNet_') # to save the weight - 'CNN_iter_'+str(i)
+	#
+
 
 if __name__ == "__main__":
 	main()
